35b1cf329ce5b8d9bd99ae513f67f9e99f25aa82
yassinedzaouha <yassinepatin@gmail.com>
2024-03-19 03:36:51 +0000

    improvement script for the better

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-function phones(phone) {
-    document.querySelector('.img').src = phone;
-}
+let img = document.querySelector(".img");
+let cont = document.querySelector(".container");
 
-function zoomIn() {
-    document.querySelector('.img').classList.add('zoomed-in');
-    setTimeout(zoomOut, 1500);
-    function zoomOut() {
-        document.querySelector('.img').classList.remove('zoomed-in');
-    }
+function phones(phone) {
+    img.src = phone;
 }
 
 function colors(color) {
-    document.querySelector('.container').style.background = color;
+    cont.style.background = color;
 }
 
-function autoZoomOut() {
-    setTimeout(zoomOut, 2000);
-}
+function zoomIn() {
+    img.classList.add('zoomed-in');
 
-window.onload = autoZoomOut;
+    setTimeout(zoomOut, 800);
+
+    function zoomOut() {
+    img.classList.remove('zoomed-in');
+    }
+
+}
