95fb3a927559b15d5c37f248d350e6e5b95033cd
yassinedzaouha <yassinepatin@gmail.com>
2024-03-22 07:14:54 +0000

    add media anad menu in size of phone

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ function zoomIn() {
     }
 
 }
+
+const menu = document.querySelector(".div_menu");
+const menu_icon = document.querySelector(".svg");
+
+function nav(){
+    menu.style.display = "block";
+    setTimeout(() => {
+        menu_icon.style.color = "transparent";
+        menu.style.width = "50%";
+        menu.style.transition = "1s";
+    }, 0);
+    
+}
+
+function menu_close() {
+    console.log('Menu close function called.');
+    const icon = document.querySelector(".aside_icon");
+    menu.style.width = "0%";
+    setTimeout(() => {menu.style.display = "none";}, 900);
+}
+
+// document.body.onclick = menu_close;
+
